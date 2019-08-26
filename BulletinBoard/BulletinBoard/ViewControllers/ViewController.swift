057d5d60939b8aabd3ea9c13fb14582a75411f50
Juan Dominguez <juan.dominguez.developer@gmail.com>
2019-08-25 22:02:22 -0700

    add empty Xcode project for working

---
+++ BulletinBoard/BulletinBoard/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BulletinBoard
+//
+//  Created by Juan Dominguez on 8/25/19.
+//  Copyright © 2019 VBC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
